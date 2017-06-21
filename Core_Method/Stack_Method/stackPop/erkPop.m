2df558b0fc211fcc2178b0646493a01d3ceb43cc
Sebastien Corner <sebcorner@oradea.csl.local>
2017-06-21 17:27:56 -0400

    driver ERK ADJ is simplified and uworks with one integrator, ERK_ADJ1and 2 are deleted, ERK_ADJ added

--- Core_Method/Stack_Method/stackPop/erkPop.m
+++ Core_Method/Stack_Method/stackPop/erkPop.m
@@ -37,6 +37,14 @@ function [ T, H, Y, Z, stack_ptr ] = erkPop( NVAR, rkS, stack_ptr )
     end
     H = chk_H( stack_ptr );
     T = chk_T( stack_ptr );
+        % for debugging
+        %TODO WE MAY WANT TO ADD THIS. The same function was inside
+        %ERK_ADJ2_Discretintegrator.
+%      if ( OPTIONS.displaySteps == true )
+%         str = ['Backtracking. Time = ', num2str(T), '; Stepsize = ', num2str(H)];
+%         disp(str);
+%      end    
+    
     Y(1:NVAR) = chk_Y( 1:NVAR, stack_ptr );
     Z(1:NVAR, 1:rkS) = chk_Z(1:NVAR, 1:rkS, stack_ptr );
     
--- Driver/Driver_ADJ/MATLODE_ERK_ADJ_Integrator.m
+++ Driver/Driver_ADJ/MATLODE_ERK_ADJ_Integrator.m
@@ -34,7 +34,7 @@
 % |Stats|: integrator statistics
 %
 %% Description
-% Driver file to solve the system y' = F(t,y) and adjoint sensitivity 
+% Driver file to solve the system y' = F(t,y) and adjoint sensitivity
 % using an Explicit Runge-Kutta (ERK) method.
 %
 % |MATLODE_ERK_ADJ_Integrator| displays the available methods
@@ -45,15 +45,15 @@
 % options configuration and adjoint sensitivity.
 %
 % |[T, Y, Sens, Quad, Mu, Stats]| = MATLODE_ERK_ADJ_Integrator(Ode_Function,
-% Time_Interval, Y0, Options)| computes the ODE solution with respect to 
+% Time_Interval, Y0, Options)| computes the ODE solution with respect to
 % the user supplied options configuration, sensitivity, quadrature, mu and
 % statisitics.
 %
 %% Example
-% For the following examples we will use Arenstorf Orbit as a toy problem 
+% For the following examples we will use Arenstorf Orbit as a toy problem
 % to illustrate |MATLODE_ERK_ADJ_Integrator| functionalities and features.
 % To initially setup Arenstorf Orbit, execute the MATLAB commands below to
-% load our input parameters into our workspace. 
+% load our input parameters into our workspace.
 %
 %   Ode_Function        = @arenstorfOrbit_Function;
 %   Ode_Jacobian        = @arenstorfOrbit_Jacobian;
@@ -82,15 +82,15 @@
 %%
 % Dr. Adrian Sandu                 | Phone: (540) 231-2193 | Email: sandu@cs.vt.edu
 %%
-% Tony D'Augustine                 | Phone: (540) 231-6186 | Email: adaug13@vt.edu 
+% Tony D'Augustine                 | Phone: (540) 231-6186 | Email: adaug13@vt.edu
 %%
 % Computational Science Laboratory | Phone: (540) 231-6186
 %% Reference
 % [1] Tony D'Augustine, Adrian Sandu. MATLODE: A MATLAB ODE Solver and
 %     Sensitivity Analysis Toolbox. Submitted to ACM TOMS.
 %
-% [2] Hong Zhang, Adrian Sandu. FATODE: a library for forward, adjoint and 
-%     tangent linear integration of ODEs, SIAM Journal on Scientific 
+% [2] Hong Zhang, Adrian Sandu. FATODE: a library for forward, adjoint and
+%     tangent linear integration of ODEs, SIAM Journal on Scientific
 %     Computing, 36(5), C504–C523, 2014
 %
 %%
@@ -99,237 +99,133 @@
 %  ©2015 Virginia Tech Intellectual Properties, Inc.
 %
 function [ Tout_FWD, Yout_FWD, Lambda, Quadrature, Mu, Stats ] = ...
- MATLODE_ERK_ADJ_Integrator( OdeFunction, Tspan, Y0, OPTIONS_U )    
-    % Display input/output parameters
-    if ( nargout == 0 && nargin == 0 )
-        fprintf('Syntax: \n');
-        fprintf( '                                  MATLODE_ERK_ADJ_Integrator \n' );
-        fprintf( '                 [ T, Y, Sens ] = MATLODE_ERK_ADJ_Integrator(Ode_Function, Time_Interval, Y0, Options) \n' );
-        fprintf( '[ T, Y, Sens, Quad, Mu, Stats ] = MATLODE_ERK_ADJ_Integrator(Ode_Function, Time_Interval, Y0, Options) \n' );
-        fprintf( '\n' );
-        return;
-    end    
+    MATLODE_ERK_ADJ_Integrator( OdeFunction, Tspan, Y0, OPTIONS_U )
+% Display input/output parameters
+if ( nargout == 0 && nargin == 0 )
+    fprintf('Syntax: \n');
+    fprintf( '                                  MATLODE_ERK_ADJ_Integrator \n' );
+    fprintf( '                 [ T, Y, Sens ] = MATLODE_ERK_ADJ_Integrator(Ode_Function, Time_Interval, Y0, Options) \n' );
+    fprintf( '[ T, Y, Sens, Quad, Mu, Stats ] = MATLODE_ERK_ADJ_Integrator(Ode_Function, Time_Interval, Y0, Options) \n' );
+    fprintf( '\n' );
+    return;
+end
+
+% Initialize 
+stack_ptr    = 0;
+% Adjoint Flags
+adjStackFlag = true;
+adjQuadFlag  = false;
+adjMuFlag    = false;
+
+% Initialize ISTATUS and RSTATUS
+ISTATUS_TOTAL = ISTATUS_Struct('default');
+
+[ OPTIONS, Coefficient ] = OPTIONS_Configuration(OPTIONS_U, 'ERK', 'ADJ', Y0, Tspan );
+OPTIONS                  = Input_Dimension(Tspan(1), Y0, OdeFunction, OPTIONS);
+
+% Adjoint Flags
+if ( ~isempty(OPTIONS.Jacp) && ~isempty(OPTIONS.Mu) )
+    if ( ~isempty(OPTIONS.QFun) && ~isempty(OPTIONS.DRDP) && ...
+            ~isempty(OPTIONS.DRDY) && ~isempty(OPTIONS.Quadrature) )
     
-
-
-    % Initialize Y
-    Y(:,1) = Y0;
-     
-    % Adjoint Flags
-    adjStackFlag = false;
-    adjQuadFlag  = false;
-    adjMuFlag    = false;
-    
-    % Initialize ISTATUS and RSTATUS
-    ISTATUS_TOTAL = ISTATUS_Struct('default');
-        
-    [ OPTIONS, Coefficient ] = OPTIONS_Configuration(OPTIONS_U, 'ERK', 'ADJ', Y0, Tspan );
-    OPTIONS                  = Input_Dimension(Tspan(1), Y0, OdeFunction, OPTIONS);
-
-    % Call ERK ADJ integration
-    if ( ~isempty(OPTIONS.Jacp) && ~isempty(OPTIONS.Mu) )
-    %    OPTIONS.NP   = size(OPTIONS.Jacp(0,Y0),2);     removed this is done in the InputDimension function   
-        % ERKADJ1 w/ Quadrature
-        if ( ~isempty(OPTIONS.QFun) && ~isempty(OPTIONS.DRDP) && ...
-                ~isempty(OPTIONS.DRDY) && ~isempty(OPTIONS.Quadrature) )
-            % Adjoint Flags
-            adjStackFlag = true;
-            adjQuadFlag  = true;
-            adjMuFlag    = true;
-            stack_ptr    = 0;
-            
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            % Call Forward and Adjoint Core Method
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %   Accounts for tspan n-dimensional array to force output at
-            %   specific times. Accumlates statistics.
-            tspanMaxSize = max(size(Tspan));
-            Yout_FWD = transpose(Y0);
-            Tout_FWD = Tspan(1);
-            FWD_ISTATUS = ISTATUS_Struct('default');
-            for interval=1:tspanMaxSize-1
-                tic;
-                [ FWD_Tout_Interval, FWD_Yout_Interval, FWD_ISTATUS_interval, FWD_RSTATUS, FWD_Ierr, stack_ptr, Quadrature ] = ...
-                    ERK_FWD_Integrator( OdeFunction,[Tspan(interval), Tspan(interval+1)], Y0, OPTIONS, Coefficient, adjStackFlag, adjQuadFlag, stack_ptr );
-                elapsedTime_FWD(interval) = toc;
-                FWD_ISTATUS = ISTATUS_Add(FWD_ISTATUS,FWD_ISTATUS_interval);
-%                 OPTIONS.Quadrature = Quadrature;
-                Tout_FWD = [Tout_FWD; transpose(FWD_Tout_Interval)];
-                Yout_FWD = [Yout_FWD; FWD_Yout_Interval];
-                Y0=transpose(Yout_FWD(end,:));
-            end
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
-            
-            % Call ERK ADJ1 Core Method w/ Quadrature
-            % disp('ERKADJ1 w/ Quadrature');
-             Tf=Tout_FWD(end);
-            Yf=Yout_FWD(end,:);
-            Lambda_Tf=OPTIONS.Lambda(Tf,Yf);
-            Mu_Tf=OPTIONS.Mu(Tf,Yf);
-            tic;
-            [ ADJ_Tout, ADJ_Yout, Lambda, Mu, ADJ_ISTATUS, ADJ_RSTATUS, ADJ_Ierr ] = ...
-                ERK_ADJ1_DiscreteIntegrator(Lambda_Tf,Mu_Tf,OPTIONS, Coefficient, stack_ptr, adjQuadFlag );     
-            elapsedTime_ADJ = toc;
-            
-        % ERKADJ1 no Quadrature
-        else
-            % Adjoint Flags
-            adjStackFlag = true;
-            adjQuadFlag  = false;
-            adjMuFlag    = true;
-            stack_ptr    = 0;
-            
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            % Call Forward and Adjoint Core Method
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %   Accounts for tspan n-dimensional array to force output at
-            %   specific times. Accumlates statistics.
-            tspanMaxSize = max(size(Tspan));
-            FWD_Yout_Interval = transpose(Y0);
-            Yout_FWD = transpose(Y0);
-            Tout_FWD = Tspan(1);
-            FWD_ISTATUS = ISTATUS_Struct('default');
-            for interval=1:tspanMaxSize-1
-                tic;
-                [ FWD_Tout_Interval, FWD_Yout_Interval, FWD_ISTATUS_interval, FWD_RSTATUS, FWD_Ierr, stack_ptr ] = ...
-                    ERK_FWD_Integrator( OdeFunction,[Tspan(interval), Tspan(interval+1)], FWD_Yout_Interval(end,:), OPTIONS, Coefficient, adjStackFlag, adjQuadFlag, stack_ptr );
-                elapsedTime_FWD(interval) = toc;
-                FWD_ISTATUS = ISTATUS_Add(FWD_ISTATUS,FWD_ISTATUS_interval);
-                Tout_FWD = [Tout_FWD FWD_Tout_Interval];
-                Yout_FWD = [Yout_FWD; FWD_Yout_Interval];
-            end
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%           
-            
-            % Call ERK ADJ1 Core Method no Quadrature
-            % disp('ERKADJ1 no Quadrature');
-            tic;
-            [ ADJ_Tout, ADJ_Yout, Lambda, Mu, ADJ_ISTATUS, ADJ_RSTATUS, ADJ_Ierr ] = ...
-                ERK_ADJ1_DiscreteIntegrator( NVAR, OPTIONS, Coefficient, stack_ptr, adjQuadFlag );             
-            elapsedTime_ADJ = toc;
-        end
+        adjQuadFlag  = true;
+        adjMuFlag    = true;
     else
-        % ERKADJ2 w/ Quadrature
-        if ( ~isempty(OPTIONS.QFun) && ~isempty(OPTIONS.DRDY) && ~isempty(OPTIONS.Quadrature) )
-            % Adjoint Flags
-            adjStackFlag = true;
-            adjQuadFlag  = true;
-            adjMuFlag    = false;
-            stack_ptr    = 0;
-            
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            % Call Forward and Adjoint Core Method
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %   Accounts for tspan n-dimensional array to force output at
-            %   specific times. Accumlates statistics.
-            tspanMaxSize = max(size(Tspan));
-            FWD_Yout_Interval = transpose(Y0);
-            Yout_FWD = transpose(Y0);
-            Tout_FWD = Tspan(1);
-            FWD_ISTATUS = ISTATUS_Struct('default');
-            for interval=1:tspanMaxSize-1
-                tic;
-                [ FWD_Tout_Interval, FWD_Yout_Interval, FWD_ISTATUS_interval, FWD_RSTATUS, FWD_Ierr, stack_ptr, Quadrature ] = ...
-                    ERK_FWD_Integrator( OdeFunction,[Tspan(interval), Tspan(interval+1)], FWD_Yout_Interval(end,:), OPTIONS, Coefficient, adjStackFlag, adjQuadFlag, stack_ptr );
-                elapsedTime_FWD(interval) = toc;
-                FWD_ISTATUS = ISTATUS_Add(FWD_ISTATUS,FWD_ISTATUS_interval);
-                OPTIONS.Quadrature = Quadrature;
-                Tout_FWD = [Tout_FWD FWD_Tout_Interval];
-                Yout_FWD = [Yout_FWD; FWD_Yout_Interval];
-            end
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%           
-            
-            % Call ERK ADJ2 Core Method no Quadrature
-            % disp('ERKADJ2 w/ Quadrature');
-            tic;
-            [ Lambda, ADJ_ISTATUS, ADJ_RSTATUS, ADJ_Ierr ] = ...
-                ERK_ADJ2_DiscreteIntegrator( lambda_Tf,Mu_Tf, OPTIONS, Coefficient, stack_ptr, adjQuadFlag );  
-            elapsedTime_ADJ = toc;
-            
-        % ERKADJ2 no Quadrature
-        else
-            % Adjoint Flags
-            adjStackFlag = true;
-            adjQuadFlag  = false;
-            adjMuFlag    = false;
-            stack_ptr    = 0;                    
-            
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            % Call Forward and Adjoint Core Method
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            %   Accounts for tspan n-dimensional array to force output at
-            %   specific times. Accumlates statistics.
-            tspanMaxSize = max(size(Tspan));
-            FWD_Yout_Interval = transpose(Y0);
-            Yout_FWD = transpose(Y0);
-            Tout_FWD = Tspan(1);
-            FWD_ISTATUS = ISTATUS_Struct('default');
-            for interval=1:tspanMaxSize-1
-                tic;
-                [ FWD_Tout_Interval, FWD_Yout_Interval, FWD_ISTATUS_interval, FWD_RSTATUS, FWD_Ierr, stack_ptr ] = ...
-                    ERK_FWD_Integrator( OdeFunction,[Tspan(interval), Tspan(interval+1)], FWD_Yout_Interval(end,:), OPTIONS, Coefficient, adjStackFlag, adjQuadFlag, stack_ptr );
-                elapsedTime_FWD(interval) = toc;
-                FWD_ISTATUS = ISTATUS_Add(FWD_ISTATUS,FWD_ISTATUS_interval);
-                Tout_FWD = [Tout_FWD FWD_Tout_Interval];
-                Yout_FWD = [Yout_FWD; FWD_Yout_Interval];
-            end
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            
-            % Call ERK ADJ2 Core Method no Quadrature
-            % disp('ERKADJ2 no Quadrature');
-            tic;
-            [ Lambda, ADJ_ISTATUS, ADJ_RSTATUS, ADJ_Ierr ] = ...
-                ERK_ADJ2_DiscreteIntegrator( NVAR, OPTIONS, Coefficient, stack_ptr, adjQuadFlag ); 
-            elapsedTime_ADJ = toc;
-        end
-    end    
-    
-    % Fold statistics
-    FWD_RSTATUS.Etime = sum(elapsedTime_FWD);
-    ADJ_RSTATUS.Etime = elapsedTime_ADJ;
-    Stats.ISTATUS_FWD = FWD_ISTATUS;
-    Stats.RSTATUS_FWD = FWD_RSTATUS;    
-    Stats.ISTATUS_ADJ = ADJ_ISTATUS;
-    Stats.RSTATUS_ADJ = ADJ_RSTATUS;
-    
-    % Display Forward Statistics
-    if ( OPTIONS.displayStats == true )
-        fprintf( '\nForward Statistics' );
-        PrintISTATUS( FWD_ISTATUS );
-        PrintRSTATUS( FWD_RSTATUS );
+
+        adjQuadFlag  = false;
+        adjMuFlag    = true;
     end
-    
-    % Display Adjoint Statistics
-    if ( OPTIONS.displayStats == true )
-        fprintf( '\n Adjoint Statistics' );
-        PrintISTATUS( ADJ_ISTATUS );
-        PrintRSTATUS( ADJ_RSTATUS );
+else
+
+    if ( ~isempty(OPTIONS.QFun) && ~isempty(OPTIONS.DRDY) && ~isempty(OPTIONS.Quadrature) )
+
+        adjQuadFlag  = true;
+        adjMuFlag    = false;
+    else
+
+        adjQuadFlag  = false;
+        adjMuFlag    = false;
     end
-    
-    ISTATUS_TOTAL.Nfun = FWD_ISTATUS.Nfun + ADJ_ISTATUS.Nfun;
-    ISTATUS_TOTAL.Njac = FWD_ISTATUS.Njac + ADJ_ISTATUS.Njac;
-    ISTATUS_TOTAL.Nstp = FWD_ISTATUS.Nstp + ADJ_ISTATUS.Nstp;
-    ISTATUS_TOTAL.Nacc = FWD_ISTATUS.Nacc + ADJ_ISTATUS.Nacc;
-    ISTATUS_TOTAL.Nrej = FWD_ISTATUS.Nrej + ADJ_ISTATUS.Nrej;
-    ISTATUS_TOTAL.Ndec = FWD_ISTATUS.Ndec + ADJ_ISTATUS.Ndec;
-    ISTATUS_TOTAL.Nsol = FWD_ISTATUS.Nsol + ADJ_ISTATUS.Nsol;
-    ISTATUS_TOTAL.Nsng = FWD_ISTATUS.Nsng + ADJ_ISTATUS.Nsng;
-    ISTATUS_TOTAL.Nchk = FWD_ISTATUS.Nchk + ADJ_ISTATUS.Nchk;    
-    
-    % Display Total Statistics
-    if ( OPTIONS.displayStats == true ) 
-        fprintf( '\n Total Statistics' );
-        PrintISTATUS( ISTATUS_TOTAL );
-    end
-            
-    % Outputs
-    if ( ~exist('Quadrature','var') )
-        Quadrature = [];
-    end
-    if ( ~exist('Mu','var')  )
-        Mu = [];
-    end
-    
-    
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Call Forward and Adjoint Core Method
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Accounts for tspan n-dimensional array to force output at
+%   specific times. Accumlates statistics.
+
+tspanMaxSize = max(size(Tspan));
+Yout_FWD = transpose(Y0);
+Tout_FWD = Tspan(1);
+FWD_ISTATUS = ISTATUS_Struct('default');
+for interval=1:tspanMaxSize-1
+    tic;
+    [ FWD_Tout_Interval, FWD_Yout_Interval, FWD_ISTATUS_interval, FWD_RSTATUS, FWD_Ierr, stack_ptr, Quadrature ] = ...
+        ERK_FWD_Integrator( OdeFunction,[Tspan(interval), Tspan(interval+1)], Y0, OPTIONS, Coefficient, adjStackFlag, adjQuadFlag, stack_ptr );
+    elapsedTime_FWD(interval) = toc;
+    FWD_ISTATUS = ISTATUS_Add(FWD_ISTATUS,FWD_ISTATUS_interval);
+    Tout_FWD = [Tout_FWD; transpose(FWD_Tout_Interval)];
+    Yout_FWD = [Yout_FWD; FWD_Yout_Interval];
+    Y0=transpose(Yout_FWD(end,:));
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Call ERK ADJ Core Method ;
+Tf=Tout_FWD(end);
+Yf=Yout_FWD(end,:);
+Lambda_Tf=OPTIONS.Lambda(Tf,Yf);
+Mu_Tf=OPTIONS.Mu(Tf,Yf);
+tic;
+[ ADJ_Tout, ADJ_Yout, Lambda, Mu, ADJ_ISTATUS, ADJ_RSTATUS, ADJ_Ierr ] = ...
+    ERK_ADJ_DiscreteIntegrator(Lambda_Tf,Mu_Tf,OPTIONS, Coefficient, stack_ptr, adjQuadFlag,adjMuFlag );
+elapsedTime_ADJ = toc;
+
+% Fold statistics
+FWD_RSTATUS.Etime = sum(elapsedTime_FWD);
+ADJ_RSTATUS.Etime = elapsedTime_ADJ;
+Stats.ISTATUS_FWD = FWD_ISTATUS;
+Stats.RSTATUS_FWD = FWD_RSTATUS;
+Stats.ISTATUS_ADJ = ADJ_ISTATUS;
+Stats.RSTATUS_ADJ = ADJ_RSTATUS;
+
+% Display Forward Statistics
+if ( OPTIONS.displayStats == true )
+    fprintf( '\nForward Statistics' );
+    PrintISTATUS( FWD_ISTATUS );
+    PrintRSTATUS( FWD_RSTATUS );
+end
+
+% Display Adjoint Statistics
+if ( OPTIONS.displayStats == true )
+    fprintf( '\n Adjoint Statistics' );
+    PrintISTATUS( ADJ_ISTATUS );
+    PrintRSTATUS( ADJ_RSTATUS );
+end
+
+ISTATUS_TOTAL.Nfun = FWD_ISTATUS.Nfun + ADJ_ISTATUS.Nfun;
+ISTATUS_TOTAL.Njac = FWD_ISTATUS.Njac + ADJ_ISTATUS.Njac;
+ISTATUS_TOTAL.Nstp = FWD_ISTATUS.Nstp + ADJ_ISTATUS.Nstp;
+ISTATUS_TOTAL.Nacc = FWD_ISTATUS.Nacc + ADJ_ISTATUS.Nacc;
+ISTATUS_TOTAL.Nrej = FWD_ISTATUS.Nrej + ADJ_ISTATUS.Nrej;
+ISTATUS_TOTAL.Ndec = FWD_ISTATUS.Ndec + ADJ_ISTATUS.Ndec;
+ISTATUS_TOTAL.Nsol = FWD_ISTATUS.Nsol + ADJ_ISTATUS.Nsol;
+ISTATUS_TOTAL.Nsng = FWD_ISTATUS.Nsng + ADJ_ISTATUS.Nsng;
+ISTATUS_TOTAL.Nchk = FWD_ISTATUS.Nchk + ADJ_ISTATUS.Nchk;
+
+% Display Total Statistics
+if ( OPTIONS.displayStats == true )
+    fprintf( '\n Total Statistics' );
+    PrintISTATUS( ISTATUS_TOTAL );
+end
+
+% Outputs
+if ( ~exist('Quadrature','var') )
+    Quadrature = [];
+end
+if ( ~exist('Mu','var')  )
+    Mu = [];
+end
+
 
 return;
 
@@ -350,7 +246,7 @@ return;
 %   </tr>
 % </table>
 % </html>
-% 
+%
 %%
 % <html>
 %   <div>
